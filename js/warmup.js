beb3e9ea28c5479959859ed76d544553ba7357d5
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-05-10 09:03:23 -0500

    Completed warmup #7

--- js/warmup.js
+++ js/warmup.js
@@ -131,6 +131,26 @@
 
     console.log("--------------------------");
 
-    // 05/08/2023 (Monday) --------------------------------------
+    // 05/10/2023 (Wednesday) --------------------------------------
+
+    function addEmUp(arr) {
+        if (Array.isArray(arr)) {
+            let sum = 0
+            for (let i = 0; i < arr.length; i++) {
+                sum+= arr[i];
+            }
+            return sum;
+        }
+    }
+
+    console.log(addEmUp([2,6,19])); // returns 27
+    console.log(addEmUp([-99, 180, -5])); // returns 76
+    console.log(addEmUp([44,10,7])); // returns 61
+    console.log(addEmUp([-100])); // returns -100
+    console.log(addEmUp([1,2,3,4,5,6,7,8,9,10])); // returns 55
+    console.log(addEmUp([-13, -92, -3500])) // returns -3605
+
+    console.log("--------------------------");
+
 })();
 
