368180d4ba7024a0eafb9265ba557b0432fa001e
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-05-12 09:12:24 -0500

    Completed warmup #9

--- js/warmup.js
+++ js/warmup.js
@@ -183,8 +183,50 @@
 
     // 05/12/2023 (Friday) --------------------------------------
 
+    const hamsters = [
+        {
+            name: "Hamtaro",
+            heightInMM: 86,
+            fur: ['orange', 'white'],
+            gender: "male",
+            dateOfBirth: "August 6"
+        } , {
+            name: "Bijou",
+            heightInMM: 75,
+            fur: ['white'],
+            gender: "female",
+            dateOfBirth: "July 10"
+        } , {
+            name: "Oxnard",
+            heightInMM: 100,
+            fur: ['grey', 'white'],
+            gender: "male",
+            dateOfBirth: "May 3"
+        } , {
+            name: "Boss",
+            heightInMM: 120,
+            fur: ['brown', 'white'],
+            gender: "male",
+            dateOfBirth: "September 21"
+        } , {
+            name: "Snoozer",
+            heightInMM: 85,
+            fur: ['brown', 'white', "pink"],
+            gender: "male",
+            dateOfBirth: "January 14"
+        }
+    ];
+    function extractNames(arr) {
+        let newArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            newArr.push(arr[i].name)
+        }
+        return newArr;
+    }
 
+    console.log(extractNames(hamsters)); // returns ["Hamtaro", "Bijou", "Oxnard", "Boss", "Snoozer"];
 
+    console.log("--------------------------");
 
 
 
