0583ddd2b4b90eb173f59eb54d741e16c1f48128
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-05-23 11:18:02 -0500

    Completed jQuery exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,8 +1,18 @@
 "use strict";
 $(function() {
+    $('.codeup').css('border', '1px solid red')
 
-    $(function () {
-        alert("The Dom Has Finished Loading")
-    });
+    $('li').css('font-size', '20px');
+
+    $('h1, p, li').css('background-color', 'yellow');
+
+    let elements = $('h1');
+    let text = '';
+
+    for (let i = 0; i < elements.length; i++) {
+        text += $(elements[i]).text() + ' ';
+    }
+    alert(text);
+
 
 });
