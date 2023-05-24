ce5d575cacaafef49a2b7c0f98862105821fb8e2
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-05-24 13:25:22 -0500

    Completed the jQuery traversing exercise, still need to do bonus

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,4 +1,5 @@
 $(function (){
+    // Exercise 1
     $('#toggle-link').click(function (event) {
         event.preventDefault();
         $('dd').toggleClass('invisible');
@@ -6,5 +7,20 @@ $(function (){
 
     $('dt').click(function() {
         $(this).toggleClass('highlighted');
+    });
+
+    // Exercise 2
+    $('#button').click(function() {
+        $('ul').each(function() {
+            $(this).find('li').last().css('background-color', '#FF0');
+        });
+    });
+
+    $('h3').click(function (){
+        $(this).nextUntil('h3').css('font-weight', 'bold');
+    });
+
+    $('li').click(function() {
+        $(this).parent().find('li:first').css('color', 'blue');
     });
 });
