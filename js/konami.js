96aa292fc64a3761021444e1ee2c4d6f82bfcc5e
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-05-23 19:11:49 -0500

    Finished positioning the divs over the image

--- js/konami.js
+++ js/konami.js
@@ -10,12 +10,77 @@ $(document).on("keyup", function(event){
         userEntry.shift()
     }
 
-    $("#keycode").html(userEntry.join(', '));
-
     if (userEntry.join() === konamiCode.join()) {
         alert("You have added 30 lives!");
     }
 });
+
+$(document).keydown(function(event) {
+    switch(event.key) {
+        case 'ArrowUp':
+            // light up the Up button
+            $('#up-button').addClass('lit');
+            break;
+        case 'ArrowDown':
+            // light up the Down button
+            $('#down-button').addClass('lit');
+            break;
+        case 'ArrowLeft':
+            // light up the Left button
+            $('#left-button').addClass('lit');
+            break;
+        case 'ArrowRight':
+            // light up the Right button
+            $('#right-button').addClass('lit');
+            break;
+        case 'b':
+            // light up the B button
+            $('#b-button').addClass('lit');
+            break;
+        case 'a':
+            // light up the A button
+            $('#a-button').addClass('lit');
+            break;
+        case 'Enter':
+            // light up the A button
+            $('#enter-button').addClass('lit');
+            break;
+    }
+});
+
+$(document).keyup(function(event) {
+    switch(event.key) {
+        case 'ArrowUp':
+            // turn off the Up button
+            $('#up-button').removeClass('lit');
+            break;
+        case 'ArrowDown':
+            // turn off the Down button
+            $('#down-button').removeClass('lit');
+            break;
+        case 'ArrowLeft':
+            // turn off the Left button
+            $('#left-button').removeClass('lit');
+            break;
+        case 'ArrowRight':
+            // turn off the Right button
+            $('#right-button').removeClass('lit');
+            break;
+        case 'b':
+            // turn off the B button
+            $('#b-button').removeClass('lit');
+            break;
+        case 'a':
+            // turn off the A button
+            $('#a-button').removeClass('lit');
+            break;
+        case 'Enter':
+            // light up the Enter button
+            $('#enter-button').removeClass('lit');
+            break;
+    }
+});
+
 
 
 
