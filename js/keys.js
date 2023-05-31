c00acf1d5f3af36af36e805673b81b200da60473
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-05-31 11:51:35 -0500

    Created file structure for weather map project

--- js/keys.js
+++ js/keys.js
@@ -1,1 +1,2 @@
-var mapBoxApiKey = 'pk.eyJ1IjoicmptZW5kZXoiLCJhIjoiY2xpNmxvZmhyMHJlNzNlcGgwMGxveDBxdCJ9.LigbIAjn0YzQUOI7Sbm-Kg'
+const mapBoxApiKey = 'pk.eyJ1IjoicmptZW5kZXoiLCJhIjoiY2xpNmxvZmhyMHJlNzNlcGgwMGxveDBxdCJ9.LigbIAjn0YzQUOI7Sbm-Kg';
+const openWeatherApiKey = '233178969c8d96ff003b814d47ba88d3';
